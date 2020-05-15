da724c1c29d5a543f9ee56a7f2c0750224b062f2
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-05-15 11:03:07 +0100

    Fix rubocop offenses

--- app/models/supplier.rb
+++ app/models/supplier.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class Supplier < ApplicationRecord
-  has_many :delivery_times
-  has_many :stock
+  has_many :delivery_times, dependent: :destroy
+  has_many :stock, dependent: :destroy
 end
