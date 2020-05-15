a55a619a71efe10a8f51a48eb31c9952e6a91832
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-05-15 12:00:20 +0100

    Add CheckoutController#allocate_basket

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -1,5 +1,33 @@
 # frozen_string_literal: true
 
 class CheckoutController < ApplicationController
-  def create; end
+  def allocate_basket
+    command = AllocateBasketCommand.new(
+      region: checkout_params[:region],
+      line_items: permitted_line_items
+    )
+
+    shipments = allocation_service.call(command)
+
+    render json: shipments.to_json
+  end
+
+  private
+
+  def checkout_params
+    params.permit(:region, line_items: [:name, :quantity])
+  end
+
+  def permitted_line_items
+    checkout_params[:line_items].map do |line_item|
+      {
+        name: line_item[:name],
+        quantity: line_item[:quantity]
+      }
+    end
+  end
+
+  def allocation_service
+    Container['allocation_service']
+  end
 end
