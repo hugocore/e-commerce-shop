fdce287ee2900e2c8c9b95c40bf3842bc806fdfe
Hugo Sequeira <hugoandresequeira@gmail.com>
2020-05-15 10:33:23 +0100

    Find delivery times for stock per region

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,4 +5,8 @@ class Stock < ApplicationRecord
   belongs_to :supplier
 
   validates :in_stock, numericality: { greater_than: 0 }
+
+  def delivery_times(region:)
+    DeliveryTime.find_by(product: product, supplier: supplier, region: region)
+  end
 end
